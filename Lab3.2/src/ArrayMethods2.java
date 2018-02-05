0c731e91c2137f292e65cfaf3229bde70c16fe1c
BT_1N3_17 <BT_1N3_17@BT_1N3_17.bktech.local>
2018-02-05 09:28:20 -0500

    asdfghjk

---
+++ Lab3.2/src/ArrayMethods2.java
@@ -0,0 +1,24 @@
+//Author: Joyin Wong
+public class ArrayMethods2 
+{
+	int[] list = {4, 8, 3, 1, 0, 2, 6, 5, 7, 9};
+	String[] list1 = {};
+	String[] list2 = {};
+	
+	public static void main(String[] args)
+	{
+		
+	}
+	
+	
+		public static int partition(int[] list)
+		{
+		
+		}
+	
+		public static String[] merge(String[] list1, String[] list2)
+		{
+			
+		}
+	
+}
