7a1a42a27077691338ca25d82c6eb9dedba01e90
BT_1N3_17 <BT_1N3_17@BT_1N3_17.bktech.local>
2018-02-06 09:29:10 -0500

    asdfk

--- Lab3.2/src/ArrayMethods2.java
+++ Lab3.2/src/ArrayMethods2.java
@@ -22,6 +22,30 @@ public class ArrayMethods2
 		{
 			int x = list1.length;
 			int y = list2.length;
+			int z = x + y;
+			String sorted[] = new String [z];
+			int a= 0;
+			int b = 0;
+			int c = 0;
+			
+			while(a < x && b < y)
+			{
+				if(list1[a].compareTo(list2[b]) > 0 )
+				{
+					sorted[c] = list1[a];
+					c++;
+					a++;
+				} else if(list1[a].compareTo(list2[b]) < 0) {
+					sort
+				}
+			}
 		}
 	
+		
+		public static void swapS(String[] arr, int index1, int index2)
+		{
+			String temp = arr[index1];
+			arr[index1] = arr[index2];
+			arr[index2] = temp;
+		}
 }
