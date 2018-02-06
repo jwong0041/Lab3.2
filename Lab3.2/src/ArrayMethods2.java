17b60dfbfe4edbfa41550dbd19768d7327dda6ff
BT_1N3_17 <BT_1N3_17@BT_1N3_17.bktech.local>
2018-02-06 09:17:45 -0500

    AAAAAAAAAAAAAA

--- Lab3.2/src/ArrayMethods2.java
+++ Lab3.2/src/ArrayMethods2.java
@@ -15,10 +15,13 @@ public class ArrayMethods2
 		{
 		
 		}
-	
+		
+		//precondition: both list1 and list2 are in alphabetical order
+	//list1 is from 0 to x //list2 is from x+1 to y
 		public static String[] merge(String[] list1, String[] list2)
 		{
-			
+			int x = list1.length;
+			int y = list2.length;
 		}
 	
 }
